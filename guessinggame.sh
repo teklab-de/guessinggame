04eb07e8c0e6d1952eed0b6bd14e80eb314f7d45
Ch.Frankenstein <ch.frankenstein@googlemail.com>
2024-11-11 01:24:51 +0100

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,25 @@
+#!/usr/bin/env bash
 
+guess_files() {
+    # Get the actual number of files in the current directory
+    correct_guess=$(ls -1 | wc -l)
+    
+    # Prompt the user for a guess
+    while true; do
+        echo "How many files are in the current directory?"
+        read user_guess
+        
+        # Check if the user's guess is correct
+        if [[ $user_guess -eq $correct_guess ]]; then
+            echo "Congratulations! You guessed the correct number of files."
+            break
+        elif [[ $user_guess -lt $correct_guess ]]; then
+            echo "Your guess is too low. Try again."
+        else
+            echo "Your guess is too high. Try again."
+        fi
+    done
+}
+
+# Call the function to start the game
+guess_files
